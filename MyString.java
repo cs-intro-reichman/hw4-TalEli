e381b32ac9505b3f8bf1623ae473da291bfb4f05
tal <tal.ariel@runi.post.ac.il>
2024-12-05 17:57:02 +0200

    hw4

--- MyString.java
+++ MyString.java
@@ -18,13 +18,39 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lowerStr = "";
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z'){
+                lowerStr += (char)(str.charAt(i)+ 32);
+            }
+            else {
+            lowerStr += str.charAt(i);
+            }
+        }
+        return lowerStr;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str2 == ""){
+            return true;
+        }
+        if (str1 == ""){
+            return false;
+        }
+        
+        for(int i =0; i<= str1.length() - str2.length(); i++){
+            int j=0;
+            while (j< str2.length() && str1.charAt(i+j) == str2.charAt(j)){
+                j++;
+            }
+
+            if (j == str2.length()){
+                return true;
+            }
+           
+        }
+        
         return false;
     }
 }
